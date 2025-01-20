91355e5b103c111ee8b95ca96f5c00a905351593
fernando <s24h@outlook.es>
2025-01-20 06:43:53 -0500

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"2ba456fd7fd03d7348f9fa0952493030c85f15
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2768582884"
+    serviceWorkerVersion: "1584556621"
   }
 });
